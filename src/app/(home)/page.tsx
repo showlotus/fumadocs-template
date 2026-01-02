cf287df328bc3049f09110a04246c0bcce773e48
showlotus <2257030399@qq.com>
2026-01-02 20:37:10 +0800

    *

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link';
+import Link from 'next/link'
+import { Cards, Card } from 'fumadocs-ui/components/card'
 
 export default function HomePage() {
   return (
@@ -6,11 +7,16 @@ export default function HomePage() {
       <h1 className="text-2xl font-bold mb-4">Hello World</h1>
       <p>
         You can open{' '}
-        <Link href="/docs" className="font-medium underline">
-          /docs
+        <Link href="/docs/components" className="font-medium underline">
+          /docs/components
         </Link>{' '}
         and see the documentation.
       </p>
+
+      <Cards className="mt-8 flex justify-center">
+        <Card title="Learn more about Next.js" href="https://nextjs.org/docs" />
+        <Card title="Learn more about Fumadocs" href="https://fumadocs.dev" />
+      </Cards>
     </div>
-  );
+  )
 }
