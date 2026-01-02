73a915c26549662ac95c96a33d8d4312a8c2cb53
showlotus <56748472+showlotus@users.noreply.github.com>
2026-01-02 12:02:05 +0000

    Deploying to gh-pages from @ showlotus/fumadocs-template@aa11ddb474a9e266fd643c76c56fde97855e4846 🚀

--- _next/static/Z5f-oN9dKpHSotzU05vZ8/_buildManifest.js
+++ _next/static/Z5f-oN9dKpHSotzU05vZ8/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/fumadocs-template//_next/:path+",
+        "destination": "/fumadocs-template/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
