0dc0866acaa8e3c2d0efdbdc2ad4bdbbaaf4ae9c
showlotus <56748472+showlotus@users.noreply.github.com>
2026-01-07 20:22:56 +0800

    feat: support local static search

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,8 +1,14 @@
 import { source } from '@/lib/source'
 import { createFromSource } from 'fumadocs-core/search/server'
 
-// TODO 静态部署不支持
-// export const { GET } = createFromSource(source, {
-//   // https://docs.orama.com/docs/orama-js/supported-languages
-//   language: 'english',
-// })
+// statically cached
+export const revalidate = false
+export const { staticGET: GET } = createFromSource(source, {
+  // https://docs.orama.com/docs/orama-js/supported-languages
+  language: 'english',
+  localeMap: {
+    // [locale]: Orama options
+    zh: { language: 'chinese' },
+    en: { language: 'english' },
+  },
+})
